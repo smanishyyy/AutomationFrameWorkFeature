1b25dbea0049b6234b172b2bcaf00db4da802a49
smanishyyy <manishchaurasia531@gmail.com>
2019-03-24 17:18:07 +0530

    add tagging

---
+++ src/test/java/com/facebook/testcase/Tc03_Tagging.java
@@ -0,0 +1,37 @@
+package com.facebook.testcase;
+
+import cucumber.api.java.en.Given;
+
+public class Tc03_Tagging {
+
+	@Given("^Launch the facebook urll$")
+	public void Given_Launch_the_facebook_urll() {
+
+	}
+
+	@Given("^Launch the facebook url22$")
+	public void Launch_the_facebook_url22() {
+
+	}
+
+	@Given("^this is a search form text$")
+	public void this_is_a_search_form_text() {
+
+	}
+
+	@Given("^this is a serach report text$")
+	public void this_is_a_serach_report_text() {
+
+	}
+
+	@Given("^user on the incident page$")
+	public void user_on_the_incident_page() {
+
+	}
+
+	@Given("^user on the change form page$")
+	public void user_on_the_change_form_page() {
+
+	}
+
+}
---
+++ target/test-classes/Features/tagging.feature
@@ -0,0 +1,27 @@
+Feature: test facbook home page funtionalaity
+
+
+@smoketest @regression
+ Scenario: Validation of static text and elemet of facebook page
+Given Launch the facebook urll
+
+@smoketest @E2E
+Scenario: ValidationOfStaticCharacterWise
+    Given Launch the facebook url22
+    
+Scenario: Search a form
+Given  this is a search form text
+
+@regression
+Scenario: Search a report
+Given this is a serach report text
+
+@E2E
+Scenario: Create a incident
+Given user on the incident page
+
+
+@E2E
+Scenario: Create a change
+Given user on the change form page
+
